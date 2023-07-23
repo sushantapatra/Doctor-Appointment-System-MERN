c7e579d7e20f0c0b2514f6a101e0a4d1d9fb7e15
sushantapatra <development.experiment@gmail.com>
2023-07-23 12:10:53 +0530

    Create Public & Private route for prevent after login user cant go login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Register from "./pages/Register";
 //get alerts slice state and show spinners
 import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
+import ProtectedRoute from "./components/ProtectedRoute";
+import PublicRoute from "./components/PublicRoute";
 function App() {
 	const { loading } = useSelector((state) => state.alerts);
 	return (
@@ -15,9 +17,30 @@ function App() {
 					<Spinner />
 				) : (
 					<Routes>
-						<Route path="/" element={<HomePage />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/register" element={<Register />} />
+						<Route
+							path="/"
+							element={
+								<ProtectedRoute>
+									<HomePage />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/login"
+							element={
+								<PublicRoute>
+									<Login />
+								</PublicRoute>
+							}
+						/>
+						<Route
+							path="/register"
+							element={
+								<PublicRoute>
+									<Register />
+								</PublicRoute>
+							}
+						/>
 					</Routes>
 				)}
 			</BrowserRouter>
